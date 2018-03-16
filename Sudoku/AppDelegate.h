ac421e49abefab0756596ad0035d7c2bfd22cad4
李一贤 <liyixian@atomlee-imac.local>
2018-03-16 09:41:09 +0800

    Initial Commit

---
+++ Sudoku/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sudoku
+//
+//  Created by 李一贤 on 2018/3/16.
+//  Copyright © 2018年 李一贤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sudoku/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sudoku
+//
+//  Created by 李一贤 on 2018/3/16.
+//  Copyright © 2018年 李一贤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
