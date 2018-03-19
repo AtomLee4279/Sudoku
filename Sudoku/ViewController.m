8f5bc78fb0feba92e4c98c234eb69508a1be3614
李一贤 <liyixian@atomlee-imac.local>
2018-03-19 15:14:03 +0800

    改用定制xib实现固定格式的子视图view内容展示

--- Sudoku/ViewController.m
+++ Sudoku/ViewController.m
@@ -47,57 +47,18 @@
         int row = i/3;
         //取出数据存进模型，为后面的控件设置具体内容作准备
         AppModel *appModel = self.apps[i];
-        //生成格子
-        UIView *appView = [[UIView alloc] init ];
-        //设置格子位置，尺寸
+        //生成格子:使用加载xib的方法
+        UIView* appView = [[[NSBundle mainBundle] loadNibNamed:@"appView" owner:nil options:nil] lastObject];
+        //设置格子位置，尺寸、背景色
         CGFloat appViewX = XMargin+col*(appviewW+spaceXMargin);
         CGFloat appViewY = YMargin+row*(appviewH+spaceYMargin);
         appView.frame = CGRectMake(appViewX,appViewY, appviewW, appviewH);
         appView.backgroundColor = [UIColor grayColor];
-        
-        //生成图片并添加进格子父控件中
-        UIImageView* imageView = [[UIImageView alloc]init];
+        //获取格子子控件并为它们赋值
+        UIImageView* imageView = appView.subviews[0];//按照xib布局顺序，subviews第一个控件是UIImageView
         imageView.image = [UIImage imageNamed:appModel.icon];
-        //设置图片位置居中于父控件
-        CGFloat imageW = 75;
-        CGFloat imageH = 75;
-        CGFloat imageX = (appviewW-imageW)*0.5;
-        CGFloat imageY = 0;
-        imageView.frame = CGRectMake(imageX, imageY, imageW, imageH);
-        imageView.backgroundColor = [UIColor redColor];
-        [appView addSubview:imageView];
-        
-        //生成文本label并添加进格子父控件中
-        UILabel* label = [[UILabel alloc]init];
+        UILabel* label = appView.subviews[1];
         label.text = appModel.name;
-        //设置文本框长度与父控件宽度一致，位置暂定为紧靠图片控件之下
-        CGFloat labelW = appviewW;
-        CGFloat labelH = 10;
-        CGFloat labelx = 0;
-        CGFloat offset = 5;//文本框相对于图片控件高度的偏移量，独立出来以便后续改需求
-        CGFloat labely = imageH+offset;
-        label.frame = CGRectMake(labelx, labely, labelW, labelH);
-        label.textColor = [UIColor whiteColor];
-        label.textAlignment = NSTextAlignmentCenter;//文本居中
-        label.font = [UIFont systemFontOfSize:18];//字体大小
-        [appView addSubview:label];
-        
-        //生成按钮btn并添加进格子父控件中
-        UIButton* btn = [[UIButton alloc]init];
-        //初定一个按钮x坐标
-        CGFloat btnX = 10;
-        //按钮与文本控件的垂直间隙
-        CGFloat spaceMargin = 5;
-        //按钮宽高
-        CGFloat btnW = appviewW -2*btnX;
-        CGFloat btnH = 10;
-        //按钮Y坐标
-        CGFloat btnY = labely+labelH+spaceMargin;
-        btn.frame = CGRectMake(btnX, btnY, btnW, btnH);
-        btn.backgroundColor = [UIColor greenColor];
-        //按钮文字
-        [btn setTitle:@"下载" forState:(UIControlStateNormal)];
-        [appView addSubview:btn];
         [self.view addSubview:appView];
     }
 }
