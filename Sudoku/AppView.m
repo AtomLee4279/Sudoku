a01b2d198abffd81fbe3fcc5dceca13a4f1ea5cd
李一贤 <liyixian@atomlee-imac.local>
2018-03-20 09:48:42 +0800

    补充重写模型类set方法的注释

--- Sudoku/AppView.m
+++ Sudoku/AppView.m
@@ -18,10 +18,10 @@
         _label.text = appModel.name;
         return; 
 }
-
+//重写AppModel的set方法
 -(void)setAppModel:(AppModel *)appModel
 {
-    //固定写法，去掉也不会影响模型赋值数据，但是会影响模型的get方法。如果调用AppModel的get方法会出问题
+    //固定写法，去掉也不会影响模型赋值数据。但是会影响模型的get方法。因为重写了AppModel的set方法，如果调用AppModel的get方法会出问题
     _appModel = appModel;
     //设置图片内容
     _image.image = [UIImage imageNamed:appModel.icon];
